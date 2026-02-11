a7c30aaab1927172b535ef1d7945375320f6a32a
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2026-02-11 14:21:38 +0530

    Add comparison and boolean operations examples

---
+++ comparisionAndBooleans.py
@@ -0,0 +1,30 @@
+print(3==3.0) #True
+print(3==2) #False
+print('hello'=='bye') #False
+print(2=='2') #False
+print('Bye'=='bye') #False
+print(2!=2) #False
+
+
+#chaining comparison operator by using logical operaror
+
+#chaining-
+print(1<2<3) #o/p:- true
+print(1<2>3) #o/p:- false
+
+print(1<2 and 2<3) #o/p:- true
+print(1<2 and 2>3) #o/p:- false
+
+print(1<2 or 2>3) #o/p:- true
+
+print(not(1==1)) #o/p:- false
+print(not 400>4000) #o/p:- true
+
+
+
+
+
+         # booleans
+
+print(1>2) #o/p:- False
+print(1==1) #o/p:- True  
