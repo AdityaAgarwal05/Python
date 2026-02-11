0d8955366847ee2cb7e2687b0ac0f4594d39498f
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2026-02-11 14:05:08 +0530

    if elif else in python

---
+++ ifelif.py
@@ -0,0 +1,17 @@
+# hungry = True
+# if hungry:
+#     print('feed')
+# else:
+#     print('i am not hungry')
+
+
+loc = ''
+
+if loc=='shop':
+    print('shop is cool')
+
+elif loc=='bank':
+    print('bank is cool')
+
+else:
+    print('nothing is cool') 
