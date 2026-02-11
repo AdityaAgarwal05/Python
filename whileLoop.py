4155d33d96a14dbe2434a0cc2a23f146bb24a4b3
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2026-02-11 14:10:34 +0530

    implementation of while loop

---
+++ whileLoop.py
@@ -0,0 +1,60 @@
+# x=0
+
+# while x<5:
+#     print(f'the value of this iteration is {x}')
+#     x+=1
+# else:
+#     print('the value of x is no longer less than 5')    
+
+# o/p:-
+# the value of this iteration is 0
+# the value of this iteration is 1
+# the value of this iteration is 2
+# the value of this iteration is 3
+# the value of this iteration is 4
+# the value of x is no longer less than 5    
+
+
+
+                              # pass keyword   -->does nothing, used to fill as placeholder where we dont want to put anything as of now
+
+# abc= [1,2,3,4,5]
+# for item in abc:
+#     #comment       --> will give an error
+
+# abc= [1,2,3,4,5]
+# for item in abc:
+#     pass           # No error
+
+
+                              #continue keyword   --> goes to the top of the closest enclosing loop
+
+# abc= 'aditya'
+
+# for item in abc:
+#     if item=='a':
+#         continue
+#     print(item)             #o/p:- dity
+
+
+                              #Break keyword         -->breaks out the current closest enclosing loop
+
+
+# abc= 'aditya'
+# for item in abc:
+#     if item=='y':
+#         break
+#     print(item)        #o/p:- adit
+
+
+# x=0
+# while x<5:
+#     if x==4:
+#       break
+#     print(f'the value of this iteration is {x}')
+#     x+=1
+
+#o/p:- the value of this iteration is 0
+# the value of this iteration is 1
+# the value of this iteration is 2
+# the value of this iteration is 3
