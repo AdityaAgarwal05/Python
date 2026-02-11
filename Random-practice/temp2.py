005f9c9df64bdc5cef0d7136db5bb4b26c363c2e
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2026-02-11 16:57:45 +0530

    Create temp2.py

---
+++ Random-practice/temp2.py
@@ -0,0 +1,70 @@
+# a = [1,2,3,4,5]
+# b= (6,7,8,9,10)
+# c= 'abc'
+# f="abc"
+# print(f)
+# d= 10
+# e=10.0
+
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# d={1:'adi', 2:"kannu"}     --> since python is dynamically typed
+# print(d[2])
+
+# a = [1,2,3,4,5]
+# for x, y in enumerate(a):
+#     print(x,y)
+
+# b= (6,7,8,9,10)
+# for x, y in enumerate(b):
+#     print(x,y)
+
+# b= (6,7,8,9,10)
+# print(list(b))  #o/p:-[6, 7, 8, 9, 10]
+# print(b)        #o/p:-(6, 7, 8, 9, 10)
+ 
+
+# m={0:10, 1:20, 2:30}
+# print(m[1])
+
+# x,y=10,20
+# x,y=y,x
+# print(x,y)
+
+# def add(x,y):
+#     return x+y
+# print(add(1,2))
+
+
+# x=int(input("Enter 1st number"))
+# y=int(input("Enter second number"))
+# print(x**y)
+
+# x=0.2
+# y=0.1
+# print(x+y) #o/p:-0.30000000000000004
+# print(x+y==0.3) #o/p:- False
+# print(x+y-0.3)  #o/p:- 5.551115123125783e-17
+# print(0.1+0.2-0.3) #o/p:- 5.551115123125783e-17
+
+#print(round(0.1+0.2-0.3, 10)) #o/p:- 0.0
+
+
+# import math
+# print(math.isclose(0.1 + 0.2, 0.3))  # True
+
+
+# from decimal import Decimal  #--> for financial calculations 
+# a = Decimal('0.1')
+# b = Decimal('0.2')
+# c = Decimal('0.3')
+# print(a + b - c)  # 0.0
+
+
+
+# print(3==3.0) #True
