2c21e8ebe5ce922cafdc899eca082683e78a2fde
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2026-02-11 15:32:13 +0530

    assessment questions

---
+++ Assessment.py
@@ -0,0 +1,44 @@
+# Q.) print all the words of string which starts from character 'a
+
+# mystring = "aditya abhishek nandan Ansh anurag joshi gupta priyanshu jain"
+# mylist = mystring.split();
+# for item in mylist:
+#     if(item[0]=='a' or item[0]=='A'):
+#         print(item)
+#         print('\n')
+
+
+# Q.) use range to print all the even numbers from 0 to 10
+
+# for num in range(0, 11):
+#     if(num%2==0):
+#         print(num)
+#         print('\n')
+
+
+# list(range(0,11,2))        
+
+
+
+# Q.) print even if length of a word in a string is even and 'odd' if it is odd-
+# mystring = "aditya abhishek nandan Ansh anurag joshi gupta priyanshu jain"
+# for item in mystring.split():
+#     if(len(item)%2==0):
+#         print('even')
+#     else:
+#         print('odd')
+
+
+# Q.) FizzBuzz problem-
+
+# for num in range(1,101):
+#     if(num%15==0):
+#         print('FizzBuzz')
+#     elif(num%5==0):
+#         print('Buzz')
+#     elif(num%3==0):
+#         print('Fizz')   
+#     else:
+#         print(num)     
+
+
